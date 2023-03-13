a350c25c3ad7622c7297d271955ba1ddd1058e3b
张天一 <zhangtianyi@everelegance.com.cn>
2023-03-13 17:30:30 +0800

    获取token的类

--- src/main/java/cn/wmrx/getaccess/start/StartGetAccessToken.java
+++ src/main/java/cn/wmrx/getaccess/start/StartGetAccessToken.java
@@ -8,6 +8,7 @@ import cn.wmrx.getaccess.model.IWechatAccount;
 import cn.wmrx.getaccess.model.MpAccessToken;
 import cn.wmrx.getaccess.util.HttpsUtil;
 import cn.wmrx.getaccess.util.JsonMapper;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -25,14 +26,18 @@ public class StartGetAccessToken {
     public static void main(String[] args) throws Exception {
         String fileName = "src/main/resources/db.properties";
         InputStream propertiesInputStream = new FileInputStream(fileName);
+        System.out.println("配置文件读取成功");
         BufferedReader bf = new BufferedReader(new InputStreamReader(propertiesInputStream));
         Properties properties = new Properties();
         properties.load(bf);
+        System.out.println("配置文件加载成功");
 
-        String resource = "configuration.xml";
-
+        String resource = properties.getProperty("configuration");
+        System.out.println("数据库配置文件名称：" + resource);
         String generateNo = properties.getProperty("generateNo");
+        System.out.println("generateNo is：" + generateNo);
         String wechatApiUrl = properties.getProperty("wechatApiUrl");
+        System.out.println("wechatApiUrl is：" + wechatApiUrl);
 
         // 定义配置文件，相对路径，文件直接放在resources目录下
 
@@ -59,22 +64,30 @@ public class StartGetAccessToken {
         IAccessTokenDAO accessTokenDAO = sqlSession.getMapper(IAccessTokenDAO.class);
 //        获取accessToken并加锁
         AccessToken accessTokenForUpdate = accessTokenDAO.selectByIdForUpdate(generateNo);
+        System.out.println("当前数据库的值是:" + JSONObject.toJSONString(accessTokenForUpdate));
+        System.out.println("当前数据库的值对应状态：" + checkEffect(accessTokenForUpdate));
+
 //        获取wechatAccountDao
         IWechatAccountDao wechatAccountDao = sqlSession.getMapper(IWechatAccountDao.class);
         IWechatAccount wechatAccount = wechatAccountDao.getConfigureByGenerateNo(generateNo);
+        System.out.println("wechatAccount is: " + JSONObject.toJSONString(wechatAccount));
 
         MpAccessToken mpAccessToken = get(wechatAccount.getAppId(), wechatAccount.getAppSecret(), wechatApiUrl);
+        System.out.println("获取微信mpAccessToken结果" + JSONObject.toJSONString(mpAccessToken));
 
 //        拿到获取的值
         AccessToken accessToken = null;
         if (0 == mpAccessToken.getErrcode()) {
+            System.out.println("获取结果成功");
+            System.out.println("结果：" + JSONObject.toJSONString(mpAccessToken));
             accessToken = new AccessToken();
             accessToken.setGenerateNo(generateNo);
             accessToken.setAccessToken(mpAccessToken.getAccess_token());
             accessToken.setExpiresIn(mpAccessToken.getExpires_in());
             accessToken.setFreshTime(System.currentTimeMillis());
+            accessTokenDAO.update(accessToken);
         }
-        accessTokenDAO.update(accessToken);
+
 
         sqlSession.commit();
         sqlSession.close();
@@ -106,4 +119,17 @@ public class StartGetAccessToken {
 
         return accessToken;
     }
+
+    private static boolean checkEffect(AccessToken accessToken) {
+
+        int expiresIn = accessToken.getExpiresIn();
+        long freshTime = accessToken.getFreshTime();
+        long currentTimeMillis = System.currentTimeMillis();
+        long remainTime = expiresIn - (currentTimeMillis - freshTime) / 1000;
+
+        System.out.println("当前系统时间：[" + currentTimeMillis + "]ms,刷新access_token时的时间：[" + freshTime + "]ms,有效时间还剩[" + remainTime + "]s");
+
+        return (remainTime - 15 * 60) > 0;
+    }
+
 }
